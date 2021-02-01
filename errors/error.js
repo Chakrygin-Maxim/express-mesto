61ddff95816562c802de279234fdfde2ab81f3d6
Chakrygin Maxim <m.chakrygin@gmail.com>
2021-02-01 16:34:29 +0300

    fix: result revue #2

--- errors/error.js
+++ errors/error.js
@@ -1,6 +1,6 @@
 function sendError(res, err) {
   let errorCode = 500;
-  if (err.name === "ValidationError") {
+  if (err.name === "ValidationError" || err.name === "CastError") {
     errorCode = 400;
   }
   res.status(errorCode).send({ message: err.message });
